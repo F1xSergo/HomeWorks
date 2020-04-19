c6e1438e16b372a4b448269b3cca1bab90b3d9ef
Sergey Khotynuyk <hot22muz@gmail.com>
2020-04-19 16:39:24 +0300

    3commit

--- sixhomework/MapMainHomeWork2.java
+++ sixhomework/MapMainHomeWork2.java
@@ -5,10 +5,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 
+
 public class MapMainHomeWork2 {
     public static void main(String[] args) {
         String filePath = readAllBytesJava("f:/git/src/edu/academy/fivehomework/Война и мир.txt");
-        String[] words = filePath.toLowerCase().replaceAll("\\,|\\.|\\(|\\?|\\!|\\|\\--|\\-|\\;","").split(" ");
+
+        String[] words = filePath.toLowerCase().replaceAll("\\,|\\.|\\(|\\?|\\!|\\|\\--|\\-|\\;", "").split(" ");
         //String[] words = filePath.toLowerCase().split(" ");
 
 
@@ -21,18 +23,29 @@ public class MapMainHomeWork2 {
             }
         }
 
-        System.out.println("\nSorted Map......By Value");
+
         Map<String, Integer> sortedMap = sortByValue(unsortMap);
-        printMap(sortedMap);
+//        // 10 самых используемых
+//            System.out.println("Sorted Map...10...By Value");
+//            List<Map.Entry<String, Integer>> list = new ArrayList<>(sortedMap.entrySet());
+//            for (int i = 0; i < 10; i++) {
+//                System.out.println(list.get(i));
 
-
-
+        System.out.println("Sorted Map......By Value");
+        for (Map.Entry<String, Integer> entry : sortedMap.entrySet()) {
+            System.out.println((entry.getKey() + " : " + entry.getValue()));
     }
+    }
+
+//    printMap(sortedMap);
+//
+//}}
+
 
     private static Map<String, Integer> sortByValue(Map<String, Integer> unsortMap) {
 
 // 1. Конвертировать карту в список карт
-        List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(unsortMap.entrySet());
+        List<Map.Entry<String, Integer>> list = new LinkedList<>(unsortMap.entrySet());
 
 // 2. Сортировать список с Collections.sort (), предоставить собственный компаратор
         Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
@@ -44,28 +57,22 @@ public class MapMainHomeWork2 {
                 } else {
                     return 0;
                 }
-//                return (o1.getValue()).compareTo(o2.getValue());
+             //   return (o1.getValue()).compareTo(o2.getValue());
             }
         });
-
 
         // 3. Зациклить отсортированный список и поместить его в новый порядок вставки Map LinkedHashMap
         Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
         for (Map.Entry<String, Integer> entry : list) {
             sortedMap.put(entry.getKey(), entry.getValue());
         }
-        return sortedMap;
-    }
-
-
-
-         public static <K, V> void printMap(Map<K, V> map) {
-             for (Map.Entry<K, V> entry : map.entrySet()) {
-
-                 System.out.println(entry.getKey() + " : " + entry.getValue());
-
-             }
-         }
+        // 10 самых используемых
+        System.out.println("Sorted Map...10...By Value");
+        for (int i = 0 ; i < 10; i++) {
+            System.out.println(list.get(i));
+        }
+            return sortedMap;
+        }
 
         private static String readAllBytesJava (String filePath){
             String content = "";
@@ -76,7 +83,11 @@ public class MapMainHomeWork2 {
             }
             return content;
         }
-
     }
 
-
+//
+//    public static <K, V> void printMap(Map<K, V> map){
+//        for (Map.Entry<K, V> entry : map.entrySet()) {
+//            System.out.println((entry.getKey() + " : " + entry.getValue()));
+//        }
+//    }
